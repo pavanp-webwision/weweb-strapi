8599f4f36012f97a7d7b181176ffcde9e3da9ea6
dev4 webwision <pavan.p@webwision.com>
2024-08-20 15:29:08 +0530

    Auth changes in experience

--- src/api/experience/controllers/experience.js
+++ src/api/experience/controllers/experience.js
@@ -7,28 +7,28 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController(
-  "api::experience.experience",
-  ({ strapi }) => ({
-    async find(ctx) {
-      const user = ctx.state.user;
+  "api::experience.experience"
+  // ({ strapi }) => ({
+  //   async find(ctx) {
+  //     const user = ctx.state.user;
 
-      if (!user) {
-        return ctx.unauthorized("You must be logged in to view experiences.");
-      }
+  //     if (!user) {
+  //       return ctx.unauthorized("You must be logged in to view experiences.");
+  //     }
 
-      // Filter experiences by the authenticated user's ID
-      // ctx.query = {
-      //     ...ctx.query,
-      //     filters: {
-      //         ...ctx.query.filters,
-      //         user: user.id,
-      //     },
-      // };
+  //     // Filter experiences by the authenticated user's ID
+  //     // ctx.query = {
+  //     //     ...ctx.query,
+  //     //     filters: {
+  //     //         ...ctx.query.filters,
+  //     //         user: user.id,
+  //     //     },
+  //     // };
 
-      // Call the default core controller find method
-      const { data, meta } = await super.find(ctx);
+  //     // Call the default core controller find method
+  //     const { data, meta } = await super.find(ctx);
 
-      return { data, meta };
-    },
-  })
+  //     return { data, meta };
+  //   },
+  // })
 );
